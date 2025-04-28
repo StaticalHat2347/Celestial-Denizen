04868c10a468e226fec51a2d828e4e54a296ce8f
stats <riangg2004@gmail.com>
2025-04-27 20:27:14 -0400

    updated ControllerImpl to implement model movements

--- src/main/java/com/comp301/a08dungeon/model/pieces/Treasure.java
+++ src/main/java/com/comp301/a08dungeon/model/pieces/Treasure.java
@@ -2,9 +2,13 @@ package com.comp301.a08dungeon.model.pieces;
 
 public class Treasure extends APiece {
 
-  public Treasure() {}
+  private final int treasure_value = 100;
+
+  public Treasure() {
+    super("Compoments", "src/main/resources/ship_components.png");
+  }
 
   public int getValue() {
-    return -1;
+    return treasure_value;
   }
 }
