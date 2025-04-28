0e51d7b2c360104ffbe8a90274713b4308a844f4
stats <riangg2004@gmail.com>
2025-04-28 17:26:37 -0400

    thank you message in main c:

--- src/main/java/com/comp301/a08dungeon/Main.java
+++ src/main/java/com/comp301/a08dungeon/Main.java
@@ -8,3 +8,9 @@ public class Main {
     Application.launch(AppLauncher.class);
   }
 }
+
+// I want to say thank you for this assignment and I had a lot of fun with it.
+// I won't be doing the Yoda part, but I hope you enjoy the extra features such as the animated background
+// and working keyboard inputs! :D 😃
+
+// thank you everyone TAs and Professor Prairie for an awesome semester.
